1fa7c59349dc72c6a72c53276d66b214c9681b0b
hi01tech <haneengfx@gmail.com>
2025-12-27 00:09:16 +0500

    fix: upgrade

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://hi01tech.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/humanoid-robotics-book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
